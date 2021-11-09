da52891b2612b8826887999e357f0447dd3bb0af
phamtanphat <phatdroid94@gmail.com>
2021-11-09 20:53:17 +0700

    future

--- app/src/main/java/com/example/rxjavarxandroid26082021/MainActivity.java
+++ app/src/main/java/com/example/rxjavarxandroid26082021/MainActivity.java
@@ -11,6 +11,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,17 +27,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        List<String> list = new ArrayList<>(Arrays.asList("A","B","C","D","E"));
+//        List<String> list = new ArrayList<>(Arrays.asList("A","B","C","D","E"));
+//
+//        // iterator đại diện cho phần tử
+//        // iterable đại diện cho list
+//        Iterable<String> iterable = list;
+//
+//        iterable.forEach(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) {
+//                Log.d("BBB",s);
+//            }
+//        });
+//        Log.d("BBB","Tiếp tục");
 
-        // iterator đại diện cho phần tử
-        // iterable đại diện cho list
-        Iterable<String> iterable = list;
+        try {
+            int[]result = handleData().get();
+            int[]result2 = handleData1().get();
+            Log.d("BBB",result[0] + "");
+            Log.d("BBB",result2[0] + "");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            Log.d("BBB",e.getMessage());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Log.d("BBB",e.getMessage());
+        }
+    }
 
-        iterable.forEach(new Consumer<String>() {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private Future<int[]> handleData() {
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        int[] a = {0};
+        Future<int[]> future = executor.submit(new Runnable() {
             @Override
-            public void accept(String s) {
-                Log.d("BBB",s);
+            public void run() {
+                Log.d("BBB",Thread.currentThread().getName());
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                a[0] = 10;
             }
-        });
+        },a);
+        return future;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private Future<int[]> handleData1() {
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        int[] a = {0};
+        Future<int[]> future = executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB",Thread.currentThread().getName());
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                a[0] = 10;
+            }
+        },a);
+        return future;
     }
 }
